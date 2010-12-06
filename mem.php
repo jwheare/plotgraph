84498ea23e3b084798c3bcd710676b9beb6f8972
jwheare <james@wheare.org>
2010-12-06 16:46:09 +0000

    Fix mem.php argument handling

--- mem.php
+++ mem.php
@@ -9,25 +9,29 @@ function format_size($size) {
     }
 }
 
+$CONF = array(
+    'lowerBound' => 0,
+    'upperBound' => 300,
+    'width' => 350,
+    'height' => 200,
+    'datapoints' => isset($_SERVER['argv']) && isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : 400,
+    'title' => isset($_SERVER['argv']) && isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2] : 'Memory Usage',
+);
+
+
 function updateChart ($mems) {
-    $lowerBound = 0;
-    $upperBound = 100;
-    $width = 350;
-    $height = 200;
-    $datapoints = isset($_SESSION['argv']) && isset($_SESSION['argv'][1]) ? $_SESSION['argv'][1] : 400;
-    $title = isset($_SESSION['argv']) && isset($_SESSION['argv'][2]) ? $_SESSION['argv'][2] : 'Memory Usage';
-    $title = isset($_SESSION['argv']) && isset($_SESSION['argv'][2]) ? $_SESSION['argv'][2] : 'Memory Usage';
+    global $CONF;
     $last = count($mems) - 1;
     $lastValue = format_size($mems[$last]);
     $filename = "http://chart.apis.google.com/chart?" . implode('&', array(
         "chxt=y",
-        "chm=t" . rawurlencode($lastValue) . ",0000FF,0,{$last},10",
-        "chxr=0,{$lowerBound},{$upperBound}",
-        "chtt=" . rawurlencode($title),
+        "chxr=0,{$CONF['lowerBound']},{$CONF['upperBound']}",
+        "chtt=" . rawurlencode($CONF['title']),
         "cht=lc",
-        "chs={$width}x{$height}",
-        "chds=" . $lowerBound * 1024 * 1024 . "," . $upperBound * 1024 * 1024,
-        "chd=t:" . implode(',', array_slice($mems, $datapoints * -1)),
+        "chs={$CONF['width']}x{$CONF['height']}",
+        "chds=" . $CONF['lowerBound'] * 1024 * 1024 . "," . $CONF['upperBound'] * 1024 * 1024,
+        "chm=t" . rawurlencode($lastValue) . ",0000FF,0,{$last},10",
+        "chd=t:" . implode(',', array_slice($mems, $CONF['datapoints'] * -1)),
     ));
     print("$filename\n");
     file_put_contents('mem.jpg', file_get_contents($filename));
